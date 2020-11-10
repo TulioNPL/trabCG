ff24e7c20108824449a98c3801871830545d421e
Eduardo Braz de Campos <64810874+eduardo-braz@users.noreply.github.com>
2020-11-10 20:38:00 -0300

    Listing 16

--- TrabCG/glwidget.cpp
+++ TrabCG/glwidget.cpp
@@ -104,22 +104,30 @@ void GLWidget::readOFFFile(const QString &fileName) {
 
 }
 
-void GLWidget :: genNormals () {
-    delete [] normals;
-    normals = new QVector3D[numVertices ];
-    for (unsigned int i = 0; i < numFaces; i++) {
-        unsigned int i1 = indices[i * 3 ];
-        unsigned int i2 = indices[i * 3 + 1];
-        unsigned int i3 = indices[i * 3 + 2];
+void GLWidget::genTexCoordsCylinder()
+{
+    delete[] texCoords;
+    texCoords = new QVector2D [numVertices];
 
-        QVector3D v1 = vertices[i1]. toVector3D ();
-        QVector3D v2 = vertices[i2]. toVector3D ();
-        QVector3D v3 = vertices[i3]. toVector3D ();
-        QVector3D faceNormal = QVector3D :: crossProduct(v2 - v1 , v3 - v1);
-        normals[i1] += faceNormal;
-        normals[i2] += faceNormal;
-        normals[i3] += faceNormal;
+    double minLim = std::numeric_limits <double>:: min();
+    double maxLim = std::numeric_limits <double>:: max();
+    QVector2D max (minLim, minLim);
+    QVector2D min (maxLim, maxLim);
+
+    for ( unsigned int i = 0; i < numVertices ; i ++) {
+        QVector2D pos = vertices[i].toVector2D();
+        max.setX(qMax(max.x(), pos.x()));
+        max.setY(qMax(max.y(), pos.y()));
+        min.setX(qMin(min.x(), pos.x()));
+        min.setY(qMin(min.y(), pos.y()));
     }
 
-    for (unsigned int i = 0; i < numVertices; i++) normals[i]. normalize ();
+    QVector2D size = max - min ;
+    for ( unsigned int i=0; i < numVertices; i++) {
+        double x = 2.0 * (vertices[i].x() - min.x() ) /
+        size.x() - 1.0;
+        texCoords[i] = QVector2D(acos(x) / M_PI ,
+        ( vertices[i].y() - min.y() ) /
+        size.y() );
+    }
 }
